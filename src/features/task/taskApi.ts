3c8003c63de6103b53e74def2cd46349ba7f28b6
Arshak <arshakbabayan100@gmail.com>
2024-07-02 16:59:23 +0400

    02.07

---
+++ src/features/task/taskApi.ts
@@ -0,0 +1,46 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { ITask } from "../../type/type";
+
+export const getTasks = createAsyncThunk("get tasks", async () => {
+    const { data } = await axios.get("http://localhost:3001/tasks");
+    return data;
+});
+
+export const getTaskById = createAsyncThunk(
+    "get taskbyid",
+    async (id: number) => {
+        const { data } = await axios.get("http://localhost:3001/tasks/" + id);
+        return data;
+    }
+);
+
+export const createTask = createAsyncThunk("create task", 
+    async (obj:ITask) => {
+    const { data } = await axios.post("http://localhost:3001/tasks", obj);
+    return data;
+});
+
+export const updateTaskById = createAsyncThunk(
+    "update task",
+    async ({id, obj}:{id: number, obj:ITask}) => {
+        const { data } = await axios.patch("http://localhost:3001/tasks/" + id, obj);
+        return data;
+    }
+);
+
+export const updateTaskByDone = createAsyncThunk(
+    "update task done",
+    async (id: number) => {
+        const { data } = await axios.patch("http://localhost:3001/tasks/done/" + id);
+        return data;
+    }
+);
+
+export const deleteTaskById = createAsyncThunk(
+    "delete task",
+    async (id: number) => {
+        const { data } = await axios.delete("http://localhost:3001/tasks/" + id);
+        return data;
+    }
+);
