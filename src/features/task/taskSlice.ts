189b6a7194266f39bee4998f371dad06998f7522
VaheGomktsyan <148968766+VaheGomktsyan@users.noreply.github.com>
2024-07-03 00:10:39 +0400

    12:10am

--- src/features/task/taskSlice.ts
+++ src/features/task/taskSlice.ts
@@ -1,3 +1,43 @@
-export {
-    
+import { createSlice } from "@reduxjs/toolkit"
+import { HYDRATE } from "next-redux-wrapper";
+import { RootState } from "../../app/store";
+
+export type Task = {
+    id: number,
+    name: string,
+    description: string,
+    done: boolean
 }
+
+const initialState: { arr: Task[], task: Task } = {
+    arr: [],
+    task: {} as Task
+}
+
+const taskSlice = createSlice({
+    name: "task",
+    initialState,
+    reducers: {
+        getTasks: (state, action) => {
+            state.arr = action.payload
+        },
+        getTaskById: (state, action) => {
+            state.task = action.payload
+        }
+    },
+    extraReducers: {
+        [HYDRATE]: (state, action) => {
+            console.log("HYDRATE", state, action.payload);
+            return {
+                ...state,
+                ...action.payload.product,
+            };
+        },
+    }
+})
+
+
+export const selectTask = (st: RootState) => st.task
+export const { getTasks, getTaskById } = taskSlice.actions
+export default taskSlice.reducer
+
